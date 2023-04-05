bc9fe8a2491b0cf9043041e2c5db1c3e24ededd0
Ali Hassan <engineralihassan@gmail.com>
2023-04-04 20:11:27 -0700

    add to cart project task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.scss']
+})
+export class TodolistComponent {
+  newTodo = '';
+  todos: string[] = ["Do sehri And Fajar prayer", "Sleep 7 hours Daily ", "6 hourse study angular", "9 hourse Study .Net"];
+  isedit: boolean = false;
+  editedtodo: string = "";
+  editedTodoindex!: number;
+  addTodo() {
+    console.log(this.newTodo);
+    if (this.newTodo == '') {
+      alert('please Enter any text ');
+    }
+    else {
+      this.todos.push(this.newTodo);
+      console.log(this.newTodo)
+      alert("Totdi is added");
+
+      this.newTodo = ' ';
+
+    }
+  }
+
+  // edit todo functions
+  editTodo(todo: string) {
+    this.isedit = true;
+    console.log("Todo is", todo);
+    this.editedtodo = todo;
+    this.editedTodoindex = this.todos.indexOf(todo);
+    console.log(this.editedTodoindex);
+
+
+    // this.editTodo = todo;
+
+  }
+  // update  final
+  updateFinal() {
+    this.todos[this.editedTodoindex] = this.editedtodo;
+    this.isedit = false;
+
+  }
+
+
+
+
+  // delete todo 
+
+  deleteTodo(todo: string) {
+
+    let index = this.todos.lastIndexOf(todo);
+    console.log(index);
+    this.todos.splice(index, 1);
+    alert("Item is deleted ");
+
+
+
+  }
+
+}
